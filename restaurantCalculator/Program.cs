d8e292a9d43827742ff7f56c9985075cce104f95
Johan <jo3@myself.com>
2019-08-25 16:03:52 -0700

    * test1.csproj: * restaurantCalculator.sln: step1
    * Program.cs: maximum 2 numbers, comma delimiter, invalid/missing
      numbers converted to 0
    
    * UnitTest1.cs: step1 unit test

--- restaurantCalculator/Program.cs
+++ restaurantCalculator/Program.cs
@@ -1,12 +1,52 @@
 ﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace restaurantCalculator
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
-            Console.WriteLine("Hello World!");
+            //#1
+
+            //read user input
+            string userInput = Console.ReadLine();
+
+            //missing input change to 0
+            if (string.IsNullOrEmpty(userInput))
+            {
+                userInput = "0";
+            }
+
+            //set delimiter
+            char[] delimiter = { ',' };
+            //put string to array
+            string[] data = userInput.Split(delimiter);
+            //remove empty string in array
+            data = data.Where(x => !string.IsNullOrEmpty(x)).ToArray();
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                //check if data is a number
+                string num = Regex.Match(data[i], @"-?\d+").Value;
+                if (num == "")
+                {
+                    data[i] = "0";
+                }
+            }
+
+            //check array length
+            if (data.Length < 2)
+            {
+                Console.Write(data[0]);
+            }
+            else
+            {
+                int total = int.Parse(data[0]) + int.Parse(data[1]);
+                //write the output
+                Console.Write($"{data[0]} + {data[1]} = {total}");
+            }
         }
     }
 }
---
+++ test1/UnitTest1.cs
@@ -0,0 +1,28 @@
+using System;
+using System.IO;
+using Xunit;
+
+namespace test1
+{
+	public class UnitTest1
+	{
+		[Fact]
+		public void Test1()
+		{
+			using (var sw = new StringWriter())
+			{
+				string input = "1,20";
+				string output = "1 + 20 = 21";
+				using (var sr = new StringReader(input))
+				{
+					Console.SetOut(sw);
+					Console.SetIn(sr);
+					restaurantCalculator.Program.Main();
+					var result = sw.ToString();
+					Assert.Equal(output, result);
+				}
+			}
+		}
+
+	}
+}
